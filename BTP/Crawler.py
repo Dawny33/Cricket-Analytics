5ff3810166bf2e32405b3c094a9dbc7d503b84ef
Dawny33 <rajrohit@iitj.ac.in>
2014-10-15 00:10:44 +0530

    Deployed the first crawler

--- BTP/Crawler.py
+++ BTP/Crawler.py
@@ -12,50 +12,50 @@ data = open('ODI_ind_vs_aus.csv','wb',)
 out = csv.writer(data)
 out.writerow(('Team', 'Result', 'Toss', 'Bat','Opposition','Ground','Date'))
 for page in url5:
-    team = []
-    result = []
-    toss = []
-    bat = []
-    opp = []
-    ground = []
-    date = []
+    teamsss = []
+    resultsss = []
+    tosssss = []
+    batsss = []
+    oppsss = []
+    groundsss = []
+    datesss = []
 
     content = lxml.html.parse(page)
     
-    team = content.xpath('//tr[@class="data1"]/td[1]')
+    team = content.xpath('//tr[@class="data1"]/td[1]/a')
     result = content.xpath('//tr[@class="data1"]/td[2]')
     toss = content.xpath('//tr[@class="data1"]/td[5]')
-    mins = content.xpath('//tr[@class="data1"]/td[3]')
-    bfs = content.xpath('//tr[@class="data1"]/td[4]')
-    fours = content.xpath('//tr[@class="data1"]/td[5]')
-    sixes = content.xpath('//tr[@class="data1"]/td[6]')
-    strike = content.xpath('//tr[@class="data1"]/td[7]')
-    inns = content.xpath('//tr[@class="data1"]/td[8]')
-    oppos = content.xpath('//tr[@class="data1"]/td[10]/a')
+    bat = content.xpath('//tr[@class="data1"]/td[6]')
+    opp = content.xpath('//tr[@class="data1"]/td[8]/a')
+    ground = content.xpath('//tr[@class="data1"]/td[9]/a')
+    date = content.xpath('//tr[@class="data1"]/td[10]/b')
+    #strike = content.xpath('//tr[@class="data1"]/td[7]')
+    #inns = content.xpath('//tr[@class="data1"]/td[8]')
+    #oppos = content.xpath('//tr[@class="data1"]/td[10]/a')
 
-    batsmen = [bat.text for bat in bats]
-    count = [c.tail for c in ct]
-    rus = [r.text for r in run]
-    mints = [mint.text for mint in mins]
-    bface = [bfaced.text for bfaced in bfs]
-    four = [fs.text for fs in fours]
-    six = [s.text for s in sixes]
-    str_rat = [srt.text for srt in strike]
-    inngs = [inn.text for inn in inns]
-    oppos = [opp.text for opp in oppos]
+    team1 = [t.text for t in team]
+    results = [r.text for r in result]
+    tosss = [tosses.text for tosses in toss]
+    bats = [first.text for first in bat]
+    opposition = [oppose.text for oppose in opp]
+    gr = [g.text for g in ground]
+    dates = [d.text for d in date]
+    #str_rat = [srt.text for srt in strike]
+    #inngs = [inn.text for inn in inns]
+    #oppos = [opp.text for opp in oppos]
 
-    bmen.extend(batsmen)
-    country.extend(count)
-    mins_crease.extend(mints)
-    runs.extend(rus)
-    balls_faced.extend(bface)
-    fours_hit.extend(four)
-    sixes_hit.extend(six)
-    strike_rate.extend(str_rat)
-    innings.extend(inngs)
-    opposition.extend(oppos)
+    teamsss.extend(team1)
+    resultsss.extend(results)
+    tosssss.extend(tosss)
+    batsss.extend(bats)
+    oppsss.extend(opposition)
+    groundsss.extend(gr)
+    datesss.extend(dates)
+    #strike_rate.extend(str_rat)
+    #innings.extend(inngs)
+    #opposition.extend(oppos)
 
-    zipped = zip(bmen,country,mins_crease,runs,balls_faced,fours_hit,sixes_hit,strike_rate,innings,opposition)
+    zipped = zip(teamsss,resultsss,tosssss,batsss,oppsss,groundsss,datesss)
     for row in zipped:
         out.writerow(row)
         zipped = None
